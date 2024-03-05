04ee17c76bfad56a53565c97566888f1f239b9ac
이윤채 <130755399+leeyoonchae@users.noreply.github.com>
2024-03-05 18:03:33 +0900

    create

---
+++ iOS/Babya/BabyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyaApp.swift
+//  Babya
+//
+//  Created by dgsw8th61 on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct BabyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
