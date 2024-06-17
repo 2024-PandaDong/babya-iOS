4f78cb0196ddd10965924684612d170cdcd33527
lsh1202 <vic071202@gmail.com>
2024-06-17 20:21:57 +0900

    :lipstick: :: UI수정

--- Projects/App/Sources/View/ProfileView.swift
+++ Projects/App/Sources/View/ProfileView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ProfileView: View {
     @Environment(\.presentationMode) var presentationMode
     
+    @State var showDiary: Bool = true
+    
     var body: some View {
         ZStack {
             Color(red: 245/255, green: 245/255, blue: 245/255)
@@ -19,82 +21,45 @@ struct ProfileView: View {
                     .frame(height: 240)
                     .foregroundStyle(Color(red: 255/255, green: 203/255, blue: 41/255))
                     .overlay {
-                        HStack {
-                            VStack(alignment: .leading, spacing: 10) {
-                                Text("박규민님 반가워요!")
-                                    .font(.system(size: 20, weight: .bold))
-                                    .padding(.vertical, 5)
-                                
-                                Text("소망이 아빠 D-Day: 86일")
-                                    .foregroundStyle(.white)
-                                    .font(.system(size: 12))
-                                
-                                HStack {
-                                    Text("나이: 25살")
-                                    Text("결혼: 3년차")
-                                }
-                                .foregroundStyle(.white)
-                                .font(.system(size: 12))
-                                
-                                HStack {
-                                    Text("산모일기: 소망이 산모일기")
-                                    Image(systemName: "lock.fill")
-                                }
-                                .foregroundStyle(.white)
-                                .font(.system(size: 12))
-                            }
-                            
+                        VStack {
                             Spacer()
                             
-                            VStack(alignment: .trailing) {
-                                Button {
-                                    self.presentationMode.wrappedValue.dismiss()
-                                } label: {
-                                    Image(systemName: "xmark")
-                                        .foregroundStyle(.black)
-                                        .font(.system(size: 20, weight: .light))
-                                }
-                                .padding(.top)
-                                
-                                Circle()
-                                    .frame(width: 100, height: 100)
-                                    .padding(.vertical, 5)
-                                
-                                Button {
+                            HStack {
+                                VStack(alignment: .leading, spacing: 10) {
+                                    Text("박규민님 반가워요!")
+                                        .font(.system(size: 20, weight: .bold))
+                                        .padding(.vertical, 5)
                                     
-                                } label: {
-                                    Rectangle()
-                                        .frame(width: 70, height: 15)
-                                        .padding(.horizontal, 15)
-                                        .foregroundStyle(.clear)
-                                        .overlay {
-                                            Rectangle().stroke(.white, lineWidth: 2)
-                                            
-                                            Text("프로필 수정하기")
-                                                .font(.system(size: 10))
-                                                .foregroundStyle(.white)
-                                        }
+                                    Text("소망이 아빠 D-Day: 86일")
+                                        .foregroundStyle(.white)
+                                        .font(.system(size: 12))
+                                    
+                                    HStack {
+                                        Text("나이: 25살")
+                                        Text("결혼: 3년차")
+                                    }
+                                    .foregroundStyle(.white)
+                                    .font(.system(size: 12))
+                                    
+                                    HStack {
+                                        Text("산모일기: 소망이 산모일기")
+                                        Image(systemName: "lock.fill")
+                                    }
+                                    .foregroundStyle(.white)
+                                    .font(.system(size: 12))
+                                }
+                                
+                                Spacer()
+                                
+                                VStack(alignment: .trailing) {
+                                    Circle()
+                                        .frame(width: 100, height: 100)
                                 }
                             }
+                            .padding(.horizontal, 40)
+                            .padding(.vertical, 25)
                         }
-                        .padding(.horizontal, 40)
                     }
-                
-                HStack {
-                    Text("이동하기")
-                        .font(.system(size: 15, weight: .bold))
-                        .padding(.horizontal, 30)
-                        .padding(.vertical, 10)
-                    
-                    Spacer()
-                }
-                
-                HStack(spacing: 30) {
-                    DocumentCell(image: "doc", title: "개인정보약관 상세")
-                    DocumentCell(image: "person.fill.questionmark", title: "문의 하러 가기")
-                    DocumentCell(image: "questionmark.folder", 
-                                 title: "질문 모음")
-                }
                 
                 HStack {
                     Text("즐겨찾기 목록")
@@ -169,6 +134,52 @@ struct ProfileView: View {
             .ignoresSafeArea()
         }
         .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    self.presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundStyle(.black)
+                        .font(.system(size: 20, weight: .light))
+                }
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Menu {
+                    Toggle(isOn: $showDiary) {
+                        Text("일일퀴즈")
+                    }
+                    NavigationLink(destination: Text("프로필 수정")) {
+                        Text("프로필 수정하기")
+                    }
+                    NavigationLink(destination: Text("프로필 수정")) {
+                        Text("개인정보 약관 상세")
+                    }
+                    NavigationLink(destination: Text("프로필 수정")) {
+                        Text("문의하기")
+                    }
+                    NavigationLink(destination: Text("프로필 수정")) {
+                        Text("FAQ")
+                    }
+                    Button(role: .destructive) {
+                        
+                    } label: {
+                        Text("로그아웃")
+                            .tint(.red)
+                    }
+                    Button(role: .destructive) {
+                        
+                    } label: {
+                        Text("탈퇴하기")
+                            .tint(.red)
+                    }
+                } label: {
+                    Image(systemName: "ellipsis")
+                        .foregroundStyle(.black)
+                }
+            }
+        }
     }
 }
 
