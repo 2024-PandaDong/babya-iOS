b3399c7a31812cea694d3e2659c512e256160a55
lsh1202 <vic071202@gmail.com>
2024-09-10 13:58:25 +0900

    :sparkles: :: policy model

---
+++ Projects/App/Sources/Model/Policy/PolicyModel.swift
@@ -0,0 +1,14 @@
+//
+//  PolicyModel.swift
+//  babya
+//
+//  Created by hyk on 9/9/24.
+//
+
+import Foundation
+
+struct PolicyListModel: Codable {
+    let policyId: Int
+    let title: String
+    let editDate: String
+}
