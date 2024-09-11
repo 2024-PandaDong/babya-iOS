fa30c51365caac72a3ce6dd6bc5da06769a963c3
lsh1202 <vic071202@gmail.com>
2024-09-12 03:03:48 +0900

    :bug: policy cell

--- Projects/App/Sources/Components/Policy/PolicyCell.swift
+++ Projects/App/Sources/Components/Policy/PolicyCell.swift
@@ -10,9 +10,7 @@ import SwiftUI
 struct PolicyCell: View {
     let title: String
     let location: String
-    let locationDetail: String
-    let startDt: String
-    let endDt: String
+    let editDate: String
     let imgUrl: String
     
     var body: some View {
@@ -26,30 +24,16 @@ struct PolicyCell: View {
                             Text(title)
                                 .font(.system(size: 16, weight: .semibold))
                             
-                            HStack {
-                                Text(location)
-                                    .font(.system(size: 14, weight: .semibold))
-                                
-                                Text(locationDetail)
-                                    .font(.system(size: 14, weight: .medium))
-                            }
+                            Text(location)
+                                .font(.system(size: 14, weight: .semibold))
                             
                             HStack {
-                                Text("\(startDt) ~ \(endDt)")
+                                Text("\(editDate)")
                                     .font(.system(size: 14, weight: .medium))
                             }
                         }
                         
                         Spacer()
-                        
-                        AsyncImage(url: URL(string: imgUrl)) { image in
-                            image
-                                .image?
-                                .resizable()
-                                .frame(width: 80, height: 80)
-                                .scaledToFit()
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
-                        }
                     }
                     .padding(.horizontal, 15)
                 }
@@ -65,7 +49,7 @@ struct PolicyCell: View {
         ZStack {
             Color.black
             
-            PolicyCell(title: "이승혁이승혁", location: "존잘", locationDetail: "알파메일", startDt: "2007-12-02", endDt: "2024-08-28", imgUrl: "https://cdn.hankyung.com/photo/201810/01.18067557.1.jpg")
+            PolicyCell(title: "이승혁이승혁", location: "존잘", editDate: "2024-09-11", imgUrl: "https://cdn.hankyung.com/photo/201810/01.18067557.1.jpg")
         }
     }
         
