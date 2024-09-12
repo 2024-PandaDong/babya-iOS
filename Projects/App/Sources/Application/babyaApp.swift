ca58413cedd312f33f5cd85c8e99cfdf6497805f
lsh1202 <vic071202@gmail.com>
2024-09-12 16:47:23 +0900

    :lipstick: :: signin view UI 수정

--- Projects/App/Sources/Application/babyaApp.swift
+++ Projects/App/Sources/Application/babyaApp.swift
@@ -14,7 +14,7 @@ struct babyaApp: App {
         WindowGroup {
             NavigationView {
                 if (LoginUserHashCache.shared.checkAccessToken() != nil) {
-                    SignInView( vm: SignInViewModel(authService: RemoteAuthService()))
+                    RootView()
                 } else {
                     QuizView(vm: QuizViewModel(quizService: RemoteQuizService()))
                 }
