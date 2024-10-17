27bc6a5349c67333949bff3c576dfea931f2673d
lsh1202 <vic071202@gmail.com>
2024-10-17 20:06:33 +0900

    :bug: profile model

--- Projects/App/Sources/Model/Profile/ProfileModel.swift
+++ Projects/App/Sources/Model/Profile/ProfileModel.swift
@@ -16,8 +16,8 @@ struct ProfileModel: Codable {
 struct MyData: Codable {
     var nickname: String = ""
     var dDay: Int?
-    var age: Int = 0
-    var marriedYears: Int = 0
+    var birthDt: String = ""
+    var marriedYears: String = ""
     var children: [Childs?]?
     var profileImg: String?
 }
@@ -27,31 +27,6 @@ struct Childs: Codable {
     var name: String? = ""
     var birthName: String? = ""
     var type: String = ""
-}
-
-struct MyDiaryResponse: Codable {
-    var status: Int = 0
-    var message: String = ""
-    var data: [MyDiaryResponseData] = []
-}
-
-struct MyDiaryResponseData: Codable {
-    var id: Int = 0
-    var title: String = ""
-    var emoji: String = ""
-    var writtenDt: String = ""
-}
-
-struct MyPostResponse: Codable {
-    var status: Int = 0
-    var message: String = ""
-    var data: [MyPostResponseData] = []
-}
-
-struct MyPostResponseData: Codable {
-    var id: Int = 0
-    var title: String = ""
-    var createdAt: String = ""
 }
 
 struct RegionModel: Codable {
