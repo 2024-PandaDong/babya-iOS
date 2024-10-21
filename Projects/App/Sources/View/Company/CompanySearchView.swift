2341a9f5bd8677c373421bcf4816cc315a243a47
leeyoonchae <yoonchae2015@gmail.com>
2024-10-21 19:29:03 +0900

    :bug: 회사 페이징 수정

--- Projects/App/Sources/View/Company/CompanySearchView.swift
+++ Projects/App/Sources/View/Company/CompanySearchView.swift
@@ -15,39 +15,49 @@ struct CompanySearchView: View {
     @StateObject var vm : CompanyViewModel = .init(companyService: RemoteCompanyService())
     @StateObject var viewModel = PolicyViewModel.shared
     @StateObject var profileVM = ProfileViewModel.shared
+    @State private var count: Int = 0
+    @State var nowPage : Int = 1
+
     
     var body: some View {
-            ZStack {
-                Color.white.ignoresSafeArea()
-                VStack(alignment: .leading ,spacing: 0) {
-                        Text("가족 친화적인 회사 리스트")
-                            .font(.system(size: 16,weight: .semibold))
-                            .foregroundStyle(.black)
-                            .padding(.top,15)
-                            .padding(.bottom,4)
-                            .padding(.horizontal)
-                    
-                        Text("\(profileVM.model.data.nickname)님과 맞는 회사를 찾아보세요")
-                            .font(.system(size: 11,weight: .semibold))
-                            .foregroundStyle(Color.gray)
-                            .padding(.horizontal)
-                            .padding(.bottom,12)
-                    
-                    ScrollView {
-                        VStack(spacing: 3) {
-                            ForEach(vm.companyList, id: \.companyId) { company in
-                                NavigationLink {
-                                    CompanyDetailView(companyId: company.companyId)
-                                        .navigationBarBackButtonHidden()
-                                } label: {
-                                    CompanyRowCell(title: company.companyName, images: (company.logoImg.first ?? "dummy") ?? "dummy", address: company.address)
-                                }
+        ZStack {
+            Color.white.ignoresSafeArea()
+            VStack(alignment: .leading ,spacing: 0) {
+                Text("가족 친화적인 회사 리스트")
+                    .font(.system(size: 16,weight: .semibold))
+                    .foregroundStyle(.black)
+                    .padding(.top,15)
+                    .padding(.bottom,4)
+                    .padding(.horizontal)
+                
+                Text("\(profileVM.model.data.nickname)님과 맞는 회사를 찾아보세요")
+                    .font(.system(size: 11,weight: .semibold))
+                    .foregroundStyle(Color.gray)
+                    .padding(.horizontal)
+                    .padding(.bottom,12)
+                
+                ScrollView {
+                    VStack(spacing: 3) {
+                        ForEach(vm.companyList, id: \.companyId) { company in
+                            NavigationLink {
+                                CompanyDetailView(companyId: company.companyId)
+                                    .navigationBarBackButtonHidden()
+                            } label: {
+                                CompanyRowCell(title: company.companyName, images: (company.logoImg.first ?? "dummy") ?? "dummy", address: company.address)
+                                    .onAppear{
+                                        count += 1
+                                        print(count)
+                                        if count % 10 == 9 {
+                                            nowPage += 1
+                                        }
+                                    }
                             }
                         }
                     }
                 }
-                Spacer()
             }
+            Spacer()
+        }
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
                 VStack(alignment: .leading) {
@@ -76,6 +86,11 @@ struct CompanySearchView: View {
         .task {
             await vm.getCompany(pageRequest: PageRequest(page: 1, size: 10))
         }
+        .onChange(of: nowPage, perform: { value in
+                Task{
+                    await vm.getCompany(pageRequest: PageRequest(page: 1 , size: count + 10))
+                }
+        })
         .onAppear {
             profileVM.getMyProfile()
         }
