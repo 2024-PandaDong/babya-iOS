219f68e527f05d83cb57a00d15510c0f5c22eb43
leeyoonchae <yoonchae2015@gmail.com>
2024-10-23 09:52:32 +0900

    :lipstick: 산모 일기 수정

--- Projects/App/Sources/View/Diary/DetailDiaryView.swift
+++ Projects/App/Sources/View/Diary/DetailDiaryView.swift
@@ -44,6 +44,68 @@ struct DetailDiaryView : View {
                                     .cornerRadius(10)
                                     .padding(.horizontal,15)
                                 }
+                                if Diary.isPublic == "N"{
+                                    VStack(alignment: .leading ,spacing: 15){
+                                        HStack{
+                                            VStack(alignment: .center,spacing: 5) {
+                                                if let emotion = Emotion.allCases.first(where: { $0.label == Diary.emojiCode }) {
+                                                    Image(emotion.rawValue)
+                                                        .resizable()
+                                                        .frame(width: 35, height: 35)
+                                                    Text(emotion.label)
+                                                        .font(.system(size: 12))
+                                                } else {
+                                                    Text("Unknown Emotion")
+                                                        .font(.system(size: 12))
+                                                }
+                                            }
+                                            Spacer()
+                                            VStack(alignment: .leading ,spacing: 10){
+                                                HStack(spacing:7){
+                                                    Text("체중:")
+                                                        .font(.system(size: 13))
+                                                    TextLineStyle(text:String( Diary.weight))
+                                                    
+                                                    Text("kg")
+                                                        .font(.system(size: 13))
+                                                        .padding(.trailing,30)
+                                                    
+                                                    Text("임신주수:")
+                                                        .font(.system(size: 13))
+                                                    TextLineStyle(text: String(Diary.pregnancyWeeks))
+                                                    Text("주")
+                                                        .font(.system(size: 13))
+                                                }
+                                                
+                                                HStack(spacing:7){
+                                                    Text("혈압:")
+                                                        .font(.system(size: 13))
+                                                    TextLineStyle(text: String(Diary.systolicPressure))
+                                                    Text("/")
+                                                        .font(.system(size: 13))
+                                                    TextLineStyle(text: String(Diary.diastolicPressure))
+                                                        .padding(.trailing,14)
+                                                    
+                                                    Text("다음 진찰일:")
+                                                        .font(.system(size: 13))
+                                                    Text(Diary.nextAppointment)
+                                                        .font(.system(size: 13))
+                                                }
+                                            }
+                        
+                                        }
+        
+                                        Text("태아소견")
+                                            .font(.system(size: 16))
+                                            .bold()
+                                        
+                                        Text(Diary.fetusComment)
+                                            .font(.system(size: 12))
+                                            .multilineTextAlignment(.leading)
+                                    }
+                                    .padding(.horizontal,15)
+                                    .padding(.vertical,10)
+                                }
                                 
                                 TextTitleStyle(Title: Text("본문"))
                                     .padding(.vertical,7)
