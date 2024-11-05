9e85383db7b4e77687da58a4d1b401c85eb67f28
leeyoonchae <yoonchae2015@gmail.com>
2024-11-05 17:10:07 +0900

    :bug:  수정

--- Projects/App/Sources/View/Diary/DiaryView.swift
+++ Projects/App/Sources/View/Diary/DiaryView.swift
@@ -23,161 +23,137 @@ struct DiaryView : View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @State var fetchDiary : Bool = false
     @State var TextVariable: String = ""
+    
     var body: some View {
-            ZStack{
-                VStack{
-                    HStack{
-                        Button {
-                            self.presentationMode.wrappedValue.dismiss()
-                        } label: {
-                            Image("arrow")
-                                .resizable()
-                                .frame(width: 20,height: 20)
-                        }
-                        Spacer()
+        ZStack{
+            VStack{
+                HStack(spacing: 23) {
+                    ZStack {
+                        Button(action: {
+                            Diary = true
+                            All = true
+                        }, label: {
+                            Text("공개")
+                                .font(.system(size: 16,weight: .semibold))
+                                .foregroundStyle(.black)
+                        })
+                        .disabled(Diary)
+                        .padding(.bottom,5)
                         
-//                        TextField("원하는 정책을 입력해주세요", text: $TextVariable)
-//                            .font(.system(size: 13))
-//                            .padding(10)
-//                            .background(Color.BackgroundStrong)
-//                            .cornerRadius(10)
-//                            .frame(height: 32)
-//                            .frame(maxWidth: .infinity)
-//                            .padding(.horizontal)
-                        
-                        
-                        Spacer()
-                        
-                        Image(systemName: "magnifyingglass")
-                            .resizable()
-                            .frame(width: 20,height: 20)
+                        Rectangle()
+                            .frame(width:42 ,height: 1.5)
+                            .foregroundColor(Diary ? Color.PrimaryNormal : .white)
+                            .padding(.top, 20)
+                            .edgesIgnoringSafeArea(.all)
                     }
-                    .padding(.top,-30)
-                    .padding(.horizontal,24)
-                    HStack(spacing: 23) {
-                        ZStack {
-                            Button(action: {
-                                Diary = true
-                                All = true
-                            }, label: {
-                                Text("공개")
-                                    .font(.system(size: 16,weight: .semibold))
-                                    .foregroundStyle(.black)
-                            })
-                            .disabled(Diary)
-                            .padding(.bottom,5)
-
-                                Rectangle()
-                                    .frame(width:42 ,height: 1.5)
-                                    .foregroundColor(Diary ? Color.PrimaryNormal : .white)
-                                    .padding(.top, 20)
-                                    .edgesIgnoringSafeArea(.all)
-                        }
-                        
-                        ZStack {
-                            Button(action: {
-                                Diary = false
-
-                            }, label: {
-                                Text("비공개")
-                                    .font(.system(size: 16,weight: .semibold))
-                                    .foregroundStyle(.black)
-                            })
-                            .disabled(!Diary)
-                            .padding(.bottom,5)
-
-                                Rectangle()
-                                    .frame(width:42 ,height: 1.5)
-                                    .foregroundColor(!Diary ? Color.PrimaryNormal : .white)
-                                    .padding(.top, 20)
-                                    .edgesIgnoringSafeArea(.all)
-                        }
-                        Spacer()
-                    }
-                    .padding(.vertical,5)
-                    .padding(.horizontal,23)
+                    //모듈의 결합 방향에 따른 테스트 종류 중 개개인의 모듈에서 테스트를 시작하고, 점차 이것들을 맞추어 테스트를 한 후에 최종적으로 프로그램의 전체 테스트를 진행하는 방식 -> 상향식 테스트
+                //인수 테스트안에서 개발 조직 안에 있는 잠재 고객에게 하는 테스트 -> 알파 테스트
+                    //프로그램과 명세서 간의 차이 -> 결함
                     
-                    ScrollView(showsIndicators: false) {
-                        if vm.diarycount != 0 {
-                            LazyVGrid(columns: columns) {
-                                ForEach((0..<vm.diarycount), id: \.self) { count in
-                                    NavigationLink(
-                                        destination: {
-                                            if Diary{
-                                                    DetailDiaryView(vm: DiaryViewModel(diaryService: RemoteDiaryService()), Diary: vm.diaryList[count])
-                                            } else {
-                                                if vm.diaryList[count].isPublic == "N" {
-                                                    DetailDiaryView(vm: DiaryViewModel(diaryService: RemoteDiaryService()), Diary: vm.diaryList[count])
-                                                }
-
-                                            }
-                                        })
-                                    {
-                                        if Diary {
-                                            if vm.diaryList[count].isPublic == "Y" {
-                                                DiaryCeil(ProfileImage:vm.diaryList[count].files.first??.url ?? "baseProfile", Title: vm.diaryList[count].title, UserName: vm.diaryList[count].nickname ?? "알수없는사용자",Date: vm.diaryList[count].writtenDt)
-                                                    .padding(.vertical, 9)
-                                                    .padding(.horizontal,10)
-                                                    .onAppear{
-                                                        print("카운트 :: \(count)")
-                                                        if count % 10 == 9 {
-                                                            nowPage += 1
-                                                        }
-                                                    }
-                                            }
-                                        }
-                                        if Diary == false {
+                    ZStack {
+                        Button(action: {
+                            Diary = false
+                            
+                        }, label: {
+                            Text("비공개")
+                                .font(.system(size: 16,weight: .semibold))
+                                .foregroundStyle(.black)
+                        })
+                        .disabled(!Diary)
+                        .padding(.bottom,5)
+                        
+                        Rectangle()
+                            .frame(width:42 ,height: 1.5)
+                            .foregroundColor(!Diary ? Color.PrimaryNormal : .white)
+                            .padding(.top, 20)
+                            .edgesIgnoringSafeArea(.all)
+                    }
+                    Spacer()
+                }
+                .padding(.vertical,5)
+                .padding(.horizontal,23)
+                
+                ScrollView(showsIndicators: false) {
+                    if vm.diarycount != 0 {
+                        LazyVGrid(columns: columns) {
+                            ForEach((0..<vm.diarycount), id: \.self) { count in
+                                NavigationLink(
+                                    destination: {
+                                        if Diary{
+                                            DetailDiaryView(vm: DiaryViewModel(diaryService: RemoteDiaryService()), Diary: vm.diaryList[count])
+                                        } else {
                                             if vm.diaryList[count].isPublic == "N" {
-                                                DiaryCeil(ProfileImage:vm.diaryList[count].files.first??.url ?? "baseProfile", Title: vm.diaryList[count].title, UserName: vm.diaryList[count].nickname ?? "알수없는사용자",Date: vm.diaryList[count].writtenDt)
-                                                    .padding(.vertical, 9)
-                                                    .padding(.horizontal,10)
-                                                    .onAppear{
-                                                        print("카운트 :: \(count)")
-                                                        if count % 10 == 9 {
-                                                            nowPage += 1
-                                                        }
-                                                    }
+                                                DetailDiaryView(vm: DiaryViewModel(diaryService: RemoteDiaryService()), Diary: vm.diaryList[count])
                                             }
+                                            
+                                        }
+                                    })
+                                {
+                                    if Diary {
+                                        if vm.diaryList[count].isPublic == "Y" {
+                                            DiaryCeil(ProfileImage:vm.diaryList[count].files.first??.url ?? "baseProfile", Title: vm.diaryList[count].title, UserName: vm.diaryList[count].nickname ?? "알수없는사용자",Date: vm.diaryList[count].writtenDt)
+                                                .padding(.vertical, 9)
+                                                .padding(.horizontal,10)
+                                                .onAppear{
+                                                    print("카운트 :: \(count)")
+                                                    if count % 10 == 9 {
+                                                        nowPage += 1
+                                                    }
+                                                }
+                                        }
+                                    }
+                                    if Diary == false {
+                                        if vm.diaryList[count].isPublic == "N" {
+                                            DiaryCeil(ProfileImage:vm.diaryList[count].files.first??.url ?? "baseProfile", Title: vm.diaryList[count].title, UserName: vm.diaryList[count].nickname ?? "알수없는사용자",Date: vm.diaryList[count].writtenDt)
+                                                .padding(.vertical, 9)
+                                                .padding(.horizontal,10)
+                                                .onAppear{
+                                                    print("카운트 :: \(count)")
+                                                    if count % 10 == 9 {
+                                                        nowPage += 1
+                                                    }
+                                                }
                                         }
                                     }
                                 }
                             }
-                        } else {
-                            VStack {
-                                Image("baseDiaryImage")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 180)
-                                    .padding(.top,50)
-                                
-                                
-                                Text("산모일기가 존재하지 않아요.")
-                                    .font(.system(size: 20, weight: .bold))
-                            }
+                        }
+                    } else {
+                        VStack {
+                            Image("baseDiaryImage")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 180)
+                                .padding(.top,50)
+                            
+                            
+                            Text("산모일기가 존재하지 않아요.")
+                                .font(.system(size: 20, weight: .bold))
                         }
                     }
                 }
-                .navigationBarBackButtonHidden()
-                .navigationBarTitleDisplayMode(.inline)
-                //                .toolbar{
-                //                    ToolbarItem(placement: .navigationBarLeading) {
-                //                        Button {
-                //                            self.presentationMode.wrappedValue.dismiss()
-                //                        } label: {
-                //                            Image("arrow")
-                //                                .resizable()
-                //                                .frame(width: 18,height: 18)
-                //                        }
-                //                    }
-                //                }
-                VStack{
+            }
+            .navigationBarBackButtonHidden()
+            .navigationBarTitleDisplayMode(.inline)
+            //                .toolbar{
+            //                    ToolbarItem(placement: .navigationBarLeading) {
+            //                        Button {
+            //                            self.presentationMode.wrappedValue.dismiss()
+            //                        } label: {
+            //                            Image("arrow")
+            //                                .resizable()
+            //                                .frame(width: 18,height: 18)
+            //                        }
+            //                    }
+            //                }
+            VStack{
+                Spacer()
+                HStack{
                     Spacer()
-                    HStack{
-                        Spacer()
-                        PencilButton()
-                            .padding()
-                    }
+                    PencilButton()
+                        .padding()
                 }
+            }
             .onChange(of: Diary) { value in
                 nowPage = 1
                 Task {
@@ -218,5 +194,21 @@ struct DiaryView : View {
             })
         }
         .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    self.presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Image("arrow")
+                        .resizable()
+                        .frame(width: 20,height: 20)
+                }
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                Image(systemName: "magnifyingglass")
+                    .resizable()
+                    .frame(width: 20,height: 20)
+            }
+        }
     }
 }
