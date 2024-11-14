4b6ea829abf452fc7ce8e75346b6dd52d87ec209
lsh1202 <vic071202@gmail.com>
2024-11-14 13:51:58 +0900

    :bug: home view

--- Projects/App/Sources/View/Home/HomeView.swift
+++ Projects/App/Sources/View/Home/HomeView.swift
@@ -7,90 +7,96 @@ struct HomeView: View {
     @StateObject var companyVM: CompanyViewModel = .init(companyService: RemoteCompanyService())
     
     var body: some View {
-        VStack {
-            ScrollView(showsIndicators: false) {
-                CustomPicker()
-                    .padding(.top,-20)
-                
-                Divider()
-                    .padding(.top, -16)
-                
-                HStack {
-                    Text("인기 있는 회사")
-                        .font(.system(size: 16, weight: .black))
+        NavigationView {
+            ZStack {
+                ScrollView(showsIndicators: false) {
+                    CustomPicker()
+                        .padding(.top,-20)
                     
-                    Spacer()
+                    Divider()
+                        .padding(.top, -16)
                     
-                    NavigationLink(destination: CompanySearchView()) {
-                        Text("더보기")
-                            .font(.system(size: 14, weight: .medium))
-                            .foregroundStyle(.black)
-                    }
-                }
-                .padding(.horizontal, 20)
-                
-                ForEach(companyVM.companyList, id: \.companyId) { company in
-                    NavigationLink {
-                        CompanyDetailView(companyId: company.companyId)
-                            .navigationBarBackButtonHidden()
-                    } label: {
-                        CompanyCell(title: company.companyName, location: company.address, image: (company.logoImg.first ?? "dummy") ?? "dummy")
-                            .padding(.horizontal)
-                    }
-                    .padding(.vertical, 5)
-                }
-                
-                Divider()
-                
-                HStack {
-                    Text("정책")
-                        .font(.system(size: 16, weight: .black))
-                    
-                    Spacer()
-                    
-                    NavigationLink(destination: PolicyView()) {
-                        Text("더보기")
-                            .font(.system(size: 14, weight: .medium))
-                            .foregroundStyle(.black)
-                    }
-                }
-                .padding(.horizontal, 20)
-                .padding(.top, 10)
-                
-                if !policyVM.model.isEmpty {
-                    ForEach(0..<3, id: \.self) { index in
-                        NavigationLink(
-                            destination: PolicyDetailView(index: policyVM.model[index].policyId)
-                        ) {
-                            PolicyCell(
-                                title: policyVM.model[index].title,
-                                location: "\(((policyVM.selectedState?.name) != nil) ? policyVM.selectedState!.name : "") \(policyVM.selectedDistrict)",
-                                editDate: policyVM.model[index].editDate ?? "", imgUrl: "", isInHome: true)
-                                .padding(.horizontal)
+                    HStack {
+                        Text("인기 있는 회사")
+                            .font(.system(size: 16, weight: .black))
+                        
+                        Spacer()
+                        
+                        NavigationLink(destination: CompanySearchView()) {
+                            Text("더보기")
+                                .font(.system(size: 14, weight: .medium))
+                                .foregroundStyle(.black)
                         }
                     }
+                    .padding(.horizontal, 20)
+                    
+                    ForEach(companyVM.companyList, id: \.companyId) { company in
+                        NavigationLink {
+                            CompanyDetailView(companyId: company.companyId)
+                                .navigationBarBackButtonHidden()
+                        } label: {
+                            CompanyCell(title: company.companyName, location: company.address, image: (company.logoImg.first ?? "dummy") ?? "dummy")
+                                .padding(.horizontal)
+                        }
+                        .padding(.vertical, 5)
+                    }
+                    
+                    Divider()
+                    
+                    HStack {
+                        Text("정책")
+                            .font(.system(size: 16, weight: .black))
+                        
+                        Spacer()
+                        
+                        NavigationLink(destination: PolicyView()) {
+                            Text("더보기")
+                                .font(.system(size: 14, weight: .medium))
+                                .foregroundStyle(.black)
+                        }
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.top, 10)
+                    
+                    if !policyVM.model.isEmpty {
+                        ForEach(0..<3, id: \.self) { index in
+                            NavigationLink(
+                                destination: PolicyDetailView(index: policyVM.model[index].policyId)
+                            ) {
+                                PolicyCell(
+                                    title: policyVM.model[index].title,
+                                    location: "\(((policyVM.selectedState?.name) != nil) ? policyVM.selectedState!.name : "") \(policyVM.selectedDistrict)",
+                                    editDate: policyVM.model[index].editDate ?? "", imgUrl: "", isInHome: true)
+                                    .padding(.horizontal)
+                            }
+                        }
+                    }
+                    
+                    Spacer()
+                        .frame(height: 100)
                 }
-                
-                Spacer()
-                    .frame(height: 100)
-            }
-            .background(Color.clear)
-        }
-        .safeAreaInset(edge: .bottom) {
-            CustomTabBar()
                 .background(Color.clear)
-                .ignoresSafeArea()
-                .padding(.bottom,-40)
+                
+                VStack {
+                    Spacer()
+                    
+                    CustomTabBar()
+                        .background(Color.clear)
+                        .padding(.bottom, -30)
+                }
+            }
+            .toolbar(.hidden)
+            .task {
+                await companyVM.getCompany(pageRequest: PageRequest(page: 1, size: 3))
+            }
+            .onAppear {
+                policyVM.model = []
+                profileVM.getMyProfile()
+                profileVM.getMyRegion()
+            }
+            .padding(.top, -40)
         }
         .navigationBarBackButtonHidden()
-        .task {
-            await companyVM.getCompany(pageRequest: PageRequest(page: 1, size: 3))
-        }
-        .onAppear {
-            policyVM.model = []
-            profileVM.getMyProfile()
-            profileVM.getMyRegion()
-        }
     }
 }
 
--- Projects/App/Sources/View/Policy/LocationView.swift
+++ Projects/App/Sources/View/Policy/LocationView.swift
@@ -20,81 +20,83 @@ struct LocationView: View {
     ]
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("지역을 선택해주세요.")
-            
-            LazyVGrid(columns: columns, spacing: 10) {
-                ForEach(LocalCode.allCases, id: \.self) { item in
-                    Button {
-                        withAnimation {
-                            viewModel.selectedState = item
-                        }
-                    } label: {
-                        Capsule()
-                            .frame(width: viewModel.calculateWidth(for: item.name), height: 25)
-                            .foregroundStyle(.clear)
-                            .overlay {
-                                Capsule().stroke(viewModel.selectedState == item ? Color.PrimaryLight : .LineAlternative, lineWidth: 1.3)
-                                
-                                Text(item.name)
-                                    .font(.system(size: 14, weight: .medium))
-                                    .foregroundStyle(viewModel.selectedState == item ? Color.PrimaryLight : .LineAlternative)
-                            }
-                    }
-                }
-            }
-            .padding(.vertical)
-            
-            Text("군을 선택해주세요.")
-            
-            
-            if let state = viewModel.selectedState, !state.country.isEmpty {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text("지역을 선택해주세요.")
+                
                 LazyVGrid(columns: columns, spacing: 10) {
-                    ForEach(Array(state.country.sorted(by: { $0.key < $1.key })), id: \.key) { (district, code) in
+                    ForEach(LocalCode.allCases, id: \.self) { item in
                         Button {
                             withAnimation {
-                                viewModel.selectedDistrict = district // 선택된 군/구의 이름 설정
-                                viewModel.regionCode = code // 선택된 군/구의 코드 설정
-                                print("Selected District: \(district), Region Code: \(code)") // 디버깅 출력
+                                viewModel.selectedState = item
                             }
                         } label: {
                             Capsule()
-                                .frame(width: viewModel.calculateWidth(for: district), height: 25)
+                                .frame(width: viewModel.calculateWidth(for: item.name), height: 25)
                                 .foregroundStyle(.clear)
                                 .overlay {
-                                    Capsule().stroke(viewModel.selectedDistrict == district ? Color.PrimaryLight : .LineAlternative, lineWidth: 1.3)
+                                    Capsule().stroke(viewModel.selectedState == item ? Color.PrimaryLight : .LineAlternative, lineWidth: 1.3)
                                     
-                                    Text(district)
+                                    Text(item.name)
                                         .font(.system(size: 14, weight: .medium))
-                                        .foregroundStyle(viewModel.selectedDistrict == district ? Color.PrimaryLight : .LineAlternative)
+                                        .foregroundStyle(viewModel.selectedState == item ? Color.PrimaryLight : .LineAlternative)
                                 }
                         }
                     }
                 }
                 .padding(.vertical)
-            }
-            
-            Spacer()
-            
-            Button {
-                self.presentationMode.wrappedValue.dismiss()
                 
-                print(viewModel.regionCode)
+                Text("군을 선택해주세요.")
                 
-                viewModel.getPolicyList(region: viewModel.regionCode, keyword: "")
-            } label: {
-                RoundedRectangle(cornerRadius: 10)
-                    .frame(width: 320, height: 45)
-                    .foregroundStyle(Color.PrimaryLight)
-                    .overlay {
-                        Text("검색하기")
-                            .foregroundStyle(.white)
+                
+                if let state = viewModel.selectedState, !state.country.isEmpty {
+                    LazyVGrid(columns: columns, spacing: 10) {
+                        ForEach(Array(state.country.sorted(by: { $0.key < $1.key })), id: \.key) { (district, code) in
+                            Button {
+                                withAnimation {
+                                    viewModel.selectedDistrict = district // 선택된 군/구의 이름 설정
+                                    viewModel.regionCode = code // 선택된 군/구의 코드 설정
+                                    print("Selected District: \(district), Region Code: \(code)") // 디버깅 출력
+                                }
+                            } label: {
+                                Capsule()
+                                    .frame(width: viewModel.calculateWidth(for: district), height: 25)
+                                    .foregroundStyle(.clear)
+                                    .overlay {
+                                        Capsule().stroke(viewModel.selectedDistrict == district ? Color.PrimaryLight : .LineAlternative, lineWidth: 1.3)
+                                        
+                                        Text(district)
+                                            .font(.system(size: 14, weight: .medium))
+                                            .foregroundStyle(viewModel.selectedDistrict == district ? Color.PrimaryLight : .LineAlternative)
+                                    }
+                            }
+                        }
                     }
+                    .padding(.vertical)
+                }
+                
+                Spacer()
+                
+                Button {
+                    self.presentationMode.wrappedValue.dismiss()
+                    
+                    print(viewModel.regionCode)
+                    
+                    viewModel.getPolicyList(region: viewModel.regionCode, keyword: "")
+                } label: {
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 320, height: 45)
+                        .foregroundStyle(Color.PrimaryLight)
+                        .overlay {
+                            Text("검색하기")
+                                .foregroundStyle(.white)
+                        }
+                }
             }
+            .padding(.vertical)
+            .font(.system(size: 16, weight: .semibold))
+            .padding(.horizontal, 30)
         }
-        .padding(.vertical)
-        .font(.system(size: 16, weight: .semibold))
-        .padding(.horizontal, 30)
     }
 }
 
