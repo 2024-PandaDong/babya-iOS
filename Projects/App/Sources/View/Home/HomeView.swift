82740cf794e1ab3aa36483b54b10aaeec05bde0e
lsh1202 <vic071202@gmail.com>
2024-11-19 12:27:09 +0900

    :bug: HomeView

--- Projects/App/Sources/View/Home/HomeView.swift
+++ Projects/App/Sources/View/Home/HomeView.swift
@@ -7,97 +7,94 @@ struct HomeView: View {
     @StateObject var companyVM: CompanyViewModel = .init(companyService: RemoteCompanyService())
     
     var body: some View {
-        NavigationView {
-            ZStack {
-                ScrollView(showsIndicators: false) {
-                    CustomPicker()
-                        .padding(.top,-20)
+        ZStack {
+            ScrollView(showsIndicators: false) {
+                CustomPicker()
+                    .padding(.top,-20)
+                
+                Divider()
+                    .padding(.top, -16)
+                
+                HStack {
+                    Text("인기 있는 회사")
+                        .font(.system(size: 16, weight: .black))
                     
-                    Divider()
-                        .padding(.top, -16)
+                    Spacer()
                     
-                    HStack {
-                        Text("인기 있는 회사")
-                            .font(.system(size: 16, weight: .black))
-                        
-                        Spacer()
-                        
-                        NavigationLink(destination: CompanySearchView()) {
-                            Text("더보기")
-                                .font(.system(size: 14, weight: .medium))
-                                .foregroundStyle(.black)
-                        }
+                    NavigationLink(destination: CompanySearchView()) {
+                        Text("더보기")
+                            .font(.system(size: 14, weight: .medium))
+                            .foregroundStyle(.black)
                     }
-                    .padding(.horizontal, 20)
+                }
+                .padding(.horizontal, 20)
+                
+                ForEach(companyVM.companyList, id: \.companyId) { company in
+                    NavigationLink {
+                        CompanyDetailView(companyId: company.companyId)
+                            .navigationBarBackButtonHidden()
+                    } label: {
+                        CompanyCell(title: company.companyName, location: company.address, image: (company.logoImg.first ?? "dummy") ?? "dummy")
+                            .padding(.horizontal)
+                    }
+                    .padding(.vertical, 5)
+                }
+                
+                Divider()
+                
+                HStack {
+                    Text("정책")
+                        .font(.system(size: 16, weight: .black))
                     
-                    ForEach(companyVM.companyList, id: \.companyId) { company in
-                        NavigationLink {
-                            CompanyDetailView(companyId: company.companyId)
-                                .navigationBarBackButtonHidden()
-                        } label: {
-                            CompanyCell(title: company.companyName, location: company.address, image: (company.logoImg.first ?? "dummy") ?? "dummy")
+                    Spacer()
+                    
+                    NavigationLink(destination: PolicyView()) {
+                        Text("더보기")
+                            .font(.system(size: 14, weight: .medium))
+                            .foregroundStyle(.black)
+                    }
+                }
+                .padding(.horizontal, 20)
+                .padding(.top, 10)
+                
+                if !policyVM.model.isEmpty {
+                    ForEach(0..<3, id: \.self) { index in
+                        NavigationLink(
+                            destination: PolicyDetailView(index: policyVM.model[index].policyId)
+                        ) {
+                            PolicyCell(
+                                title: policyVM.model[index].title,
+                                location: "\(((policyVM.selectedState?.name) != nil) ? policyVM.selectedState!.name : "") \(policyVM.selectedDistrict)",
+                                editDate: policyVM.model[index].editDate ?? "", imgUrl: "", isInHome: true)
                                 .padding(.horizontal)
                         }
-                        .padding(.vertical, 5)
                     }
-                    
-                    Divider()
-                    
-                    HStack {
-                        Text("정책")
-                            .font(.system(size: 16, weight: .black))
-                        
-                        Spacer()
-                        
-                        NavigationLink(destination: PolicyView()) {
-                            Text("더보기")
-                                .font(.system(size: 14, weight: .medium))
-                                .foregroundStyle(.black)
-                        }
-                    }
-                    .padding(.horizontal, 20)
-                    .padding(.top, 10)
-                    
-                    if !policyVM.model.isEmpty {
-                        ForEach(0..<3, id: \.self) { index in
-                            NavigationLink(
-                                destination: PolicyDetailView(index: policyVM.model[index].policyId)
-                            ) {
-                                PolicyCell(
-                                    title: policyVM.model[index].title,
-                                    location: "\(((policyVM.selectedState?.name) != nil) ? policyVM.selectedState!.name : "") \(policyVM.selectedDistrict)",
-                                    editDate: policyVM.model[index].editDate ?? "", imgUrl: "", isInHome: true)
-                                    .padding(.horizontal)
-                            }
-                        }
-                    }
-                    
-                    Spacer()
-                        .frame(height: 100)
                 }
-                .background(Color.clear)
                 
-                VStack {
-                    Spacer()
-                    
-                    CustomTabBar()
-                        .background(Color.clear)
-                        .padding(.bottom, -30)
-                }
+                Spacer()
+                    .frame(height: 100)
             }
-            .toolbar(.hidden)
-            .toolbarBackground(.hidden)
-            .task {
-                await companyVM.getCompany(pageRequest: PageRequest(page: 1, size: 3))
+            .background(Color.clear)
+            
+            VStack {
+                Spacer()
+                
+                CustomTabBar()
+                    .background(Color.clear)
+                    .padding(.bottom, -30)
             }
-            .onAppear {
-                policyVM.model = []
-                profileVM.getMyProfile()
-                profileVM.getMyRegion()
-            }
-            .padding(.top, -40)
         }
         .navigationBarBackButtonHidden()
+        .toolbarBackground(.hidden)
+        .task {
+            await companyVM.getCompany(pageRequest: PageRequest(page: 1, size: 3))
+        }
+        .onAppear {
+            policyVM.model = []
+            profileVM.getMyProfile()
+            profileVM.getMyRegion()
+        }
+        .padding(.top, -40)
     }
 }
 
